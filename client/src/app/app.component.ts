0acd047553150de74b5d2e311796b30bba89c1ed
Dhirendra Kumar <dhiru.m25@gmail.com>
2025-03-09 18:37:32 +0530

    Add CORS support and fetch users in Angular app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'client';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (data) => {
+        this.users = data;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+  }
 }
